841ab72452d60b948b5102c53c26af6a10490269
Dave <crazeydave@users.noreply.github.com>
2026-02-14 22:38:10 +0000

    registration of gsr attendee at intergroup.

--- src/IntergroupMeetings/Interfaces/IntergroupMeeting.php
+++ src/IntergroupMeetings/Interfaces/IntergroupMeeting.php
@@ -36,4 +36,28 @@ interface IntergroupMeeting
      * @return string Date in format Y-m-d or empty string if not set
      */
     public function getDate(): string;
+
+    /**
+     * Add a member ID to the group attendees list
+     *
+     * @param int $memberId
+     * @return bool True if the member was added, false if already present
+     */
+    public function addGroupAttendee(int $memberId): bool;
+
+    /**
+     * Remove a member ID from the group attendees list
+     *
+     * @param int $memberId
+     * @return bool True if the member was removed, false if not present
+     */
+    public function removeGroupAttendee(int $memberId): bool;
+
+    /**
+     * Check if a member ID is in the group attendees list
+     *
+     * @param int $memberId
+     * @return bool
+     */
+    public function hasGroupAttendee(int $memberId): bool;
 }
