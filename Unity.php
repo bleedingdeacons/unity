72447f1375f16739854e0e81e7a07235c3940884
Dave <crazeydave@users.noreply.github.com>
2026-03-12 17:30:50 +0000

    bump due to updated group email.

--- Unity.php
+++ Unity.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * Plugin Name: Unity
  * Description: An intergroup management plugin.
- * Version: 1.7.1
+ * Version: 1.7.2
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author: The Bleeding Deacons
