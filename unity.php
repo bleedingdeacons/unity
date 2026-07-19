1d9c75aad71c19a14777c360b9e1fc4c52656f48
Dave <crazeydave@users.noreply.github.com>
2026-07-19 21:35:22 +0100

    Release 1.18.3

--- unity.php
+++ unity.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * Plugin Name: Unity
  * Description: An intergroup management plugin.
- * Version: 1.18.2
+ * Version: 1.18.3
  * Requires at least: 6.1
  * Requires PHP: 8.1
  * GitHub Plugin URI: https://github.com/thebleedingdeacons/unity
