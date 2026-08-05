e0c11ac21c77d0275e5c4f69511b4d30806fb6d6
Dave <crazeydave@users.noreply.github.com>
2026-08-05 23:25:49 +0100

    chore(release): v1.22.3 (#48)

--- unity.php
+++ unity.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Unity
  * Description: An intergroup management plugin.
- * Version: 1.22.2
+ * Version: 1.22.3
  * Requires at least: 6.1
  * Requires PHP: 8.1
  * GitHub Plugin URI: https://github.com/bleedingdeacons/unity
